70f2c00f283219605e73e1677febab4594a7fec6
Saeed Rasooli <saeed.gnu@gmail.com>
2019-10-09 23:28:21 +0330

    backport jalali.py and jalali_test.py from master branch

--- scal2/cal_types/jalali.py
+++ scal2/cal_types/jalali.py
@@ -20,57 +20,79 @@
 # Also avalable in /usr/share/common-licenses/GPL on Debian systems
 # or /usr/share/licenses/common/GPL3/license.txt on ArchLinux
 
-## Iranian (Jalali) calendar:
-## http://en.wikipedia.org/wiki/Iranian_calendar
+# Iranian (Jalali) calendar:
+# http://en.wikipedia.org/wiki/Iranian_calendar
 
-name = 'jalali'
-desc = 'Jalali'
-origLang = 'fa'
+name = "jalali"
+desc = "Jalali"
+origLang = "fa"
+
+ALG33 = 0
+ALG2820 = 1
 
 monthNameMode = 0
 jalaliAlg = 0
 options = (
 	(
-		'monthNameMode',
+		"monthNameMode",
 		list,
-		'Jalali Month Names',
-		('Iranian', 'Kurdish', 'Dari', 'Pashto'),
+		"Jalali Month Names",
+		("Iranian", "Kurdish", "Dari", "Pashto"),
 	),
 	(
-		'jalaliAlg',
+		"jalaliAlg",
 		list,
-		'Jalali Calculation Algorithm',
-		('33 year algorithm', '2820 year algorithm'),
+		"Jalali Calculation Algorithm",
+		("33 year algorithm", "2820 year algorithm"),
 	),
 )
 
 
 monthNameVars = (
 	(
-		('Farvardin','Ordibehesht','Khordad','Teer','Mordad','Shahrivar',
-		 'Mehr','Aban','Azar','Dey','Bahman','Esfand'),
-		('Far', 'Ord', 'Khr', 'Tir', 'Mor', 'Shr',
-		 'Meh', 'Abn', 'Azr', 'Dey', 'Bah', 'Esf'),
+		(
+			"Farvardin", "Ordibehesht", "Khordad",
+			"Teer", "Mordad", "Shahrivar",
+			"Mehr", "Aban", "Azar",
+			"Dey", "Bahman", "Esfand",
+		),
+		(
+			"Far", "Ord", "Khr",
+			"Tir", "Mor", "Shr",
+			"Meh", "Abn", "Azr",
+			"Dey", "Bah", "Esf",
+		),
 	),
 	(
-		('Xakelêwe','Gullan','Cozerdan','Pûşper','Gelawêj','Xermanan',
-		 'Rezber','Gelarêzan','Sermawez','Befranbar','Rêbendan','Reşeme'),
+		(
+			"Xakelêwe", "Gullan", "Cozerdan",
+			"Pûşper", "Gelawêj", "Xermanan",
+			"Rezber", "Gelarêzan", "Sermawez",
+			"Befranbar", "Rêbendan", "Reşeme",
+		),
 	),
 	(
-		('Hamal','Sawr','Jawzā','Saratān','Asad','Sonbola',
-		 'Mizān','Aqrab','Qaws','Jadi','Dalvæ','Hūt'),
+		(
+			"Hamal", "Sawr", "Jawzā",
+			"Saratān", "Asad", "Sonbola",
+			"Mizān", "Aqrab", "Qaws",
+			"Jadi", "Dalvæ", "Hūt",
+		),
 	),
 	(
-		('Wray','Ǧwayay','Ǧbargolay','Čungāx̌','Zmaray','Waǵay',
-		 'Təla','Laṛam','Līndəi','Marǧūmay','Salwāǧa','Kab'),
+		(
+			"Wray", "Ǧwayay", "Ǧbargolay",
+			"Čungāx̌", "Zmaray", "Waǵay",
+			"Təla", "Laṛam", "Līndəi",
+			"Marǧūmay", "Salwāǧa", "Kab",
+		),
 	),
 )
 
-#		('','','','','','',
-#		 '','','','','','')
 
+def getMonthName(m, y=None):
+	return monthNameVars[monthNameMode][0][m - 1]
 
-getMonthName = lambda m, y=None: monthNameVars[monthNameMode][0][m-1]
 
 def getMonthNameAb(m, y=None):
 	v = monthNameVars[monthNameMode]
@@ -78,17 +100,17 @@ def getMonthNameAb(m, y=None):
 		l = v[1]
 	except IndexError:
 		l = v[0]
-	return l[m-1]
+	return l[m - 1]
 
 
-
-getMonthsInYear = lambda y: 12
+def getMonthsInYear(y):
+	return 12
 
 
 epoch = 1948321
 minMonthLen = 29
 maxMonthLen = 31
-avgYearLen = 365.2425 ## FIXME
+avgYearLen = 365.2425  # FIXME
 
 GREGORIAN_EPOCH = 1721426
 monthLen = (31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30)
@@ -97,8 +119,8 @@ monthLenSum = [0]
 for i in range(12):
 	monthLenSum.append(monthLenSum[-1] + monthLen[i])
 
-#print(monthLenSum)
-## monthLenSum[i] == sum(monthLen[:i])
+# print(monthLenSum)
+# monthLenSum[i] == sum(monthLen[:i])
 
 import os
 from bisect import bisect_left
@@ -106,6 +128,7 @@ from bisect import bisect_left
 from scal2.path import sysConfDir, confDir
 from scal2.utils import iceil
 from scal2.utils import myRaise
+from scal2.json_utils import *
 
 ## Here load user options(jalaliAlg) from file
 sysConfPath = '%s/%s.conf'%(sysConfDir, name)
@@ -133,98 +156,120 @@ def save():## Here save user options to file
 
 
 def isLeap(year):
-	"isLeap: Is a given year a leap year in the Jalali calendar ?"
+	"""
+	isLeap: Is a given year a leap year in the Jalali calendar ?
+	"""
 	alg = jalaliAlg
 	# 2820-years does not work with year < 1
 	if year < 1:
-		alg = 0 # # 33-years
-	if alg==1:## 2820-years
-		return (( (year - 473 - (year>0)) % 2820) * 682) % 2816 < 682
-	elif alg==0:## 33-years
+		alg = ALG33
+	if alg == ALG2820:
+		return (((year - 473 - (year > 0)) % 2820) * 682) % 2816 < 682
+	elif alg == ALG33:
 		jy = year - 979
-		gdays = ( 365*jy + (jy//33)*8 + (jy%33+3)//4 + 79 ) % 146097
-		## 36525 = 365*100 + 100//4
-		if gdays >= 36525:
-			gdays = (gdays-1) % 36524 + 1
-			if gdays < 366:
-				return False
-		if gdays % 1461 >= 366:
-			return False
-		return True
-
+		jyd, jym = divmod(jy, 33)
+		jyd2, jym2 = divmod(jy + 1, 33)
+		return 1 == (
+			(jyd2 - jyd) * 8
+			+ (jym2 + 3) // 4
+			- (jym + 3) // 4
+		)
 	else:
-		raise RuntimeError('bad option alg=%s'%alg)
+		raise RuntimeError("bad option alg=%s" % alg)
+
 
 def getMonthDayFromYdays(yday):
 	month = bisect_left(monthLenSum, yday)
 	day = yday - monthLenSum[month - 1]
 	return month, day
 
+
 def to_jd(year, month, day):
-	"TO_JD: Determine Julian day from Jalali date"
+	"""
+	calculate Julian day from Jalali date
+	"""
 	alg = jalaliAlg
 	# 2820-years does not work with year < 1
 	if year < 1:
-		alg = 0 # # 33-years
-	if alg==1:## 2820-years
-		epbase = year - 474 if year>=0 else 473
+		alg = ALG33
+	if alg == ALG2820:
+		epbase = year - 474 if year >= 0 else 473
 		epyear = 474 + epbase % 2820
-		return day + \
-			(month-1) * 30 + min(6, month-1) + \
-			(epyear * 682 - 110) // 2816 + \
-			(epyear - 1) * 365 + \
-			epbase // 2820 * 1029983 + \
-			epoch - 1
-	elif alg==0:## 33-years
-		y2 = year - 979
-		jdays = 365*y2 + y2//33 * 8 + (y2%33+3)//4 + monthLenSum[month-1] + (day-1)
-		return jdays + 584101 + GREGORIAN_EPOCH
+		return (
+			day
+			+ (month - 1) * 30 + min(6, month - 1)
+			+ (epyear * 682 - 110) // 2816
+			+ (epyear - 1) * 365
+			+ epbase // 2820 * 1029983
+			+ epoch
+			- 1
+		)
+	elif alg == ALG33:
+		jy = year - 979
+		jyd, jym = divmod(jy, 33)
+		return (
+			365 * jy
+			+ jyd * 8
+			+ (jym + 3) // 4
+			+ monthLenSum[month - 1]
+			+ day
+			- 1
+			+ 584101
+			+ GREGORIAN_EPOCH
+		)
 	else:
-		raise RuntimeError('bad option alg=%s'%alg)
+		raise RuntimeError("bad option alg=%s" % alg)
+
 
 def jd_to(jd):
-	"JD_TO_JALALI: Calculate Jalali date from Julian day"
+	"""
+	calculate Jalali date from Julian day
+	"""
 	alg = jalaliAlg
 	# 2820-years does not work with year < 1
 	# date=1/1/1  ->  jd=1948321
 	if jd < 1948321:
-		alg = 0 # # 33-years
-	if alg==1:## 2820-years
+		alg = ALG33
+	if alg == ALG2820:
 		cycle, cyear = divmod(jd - to_jd(475, 1, 1), 1029983)
-		if cyear == 1029982 :
+		if cyear == 1029982:
 			ycycle = 2820
 		else:
 			aux1, aux2 = divmod(cyear, 366)
-			ycycle = (2134*aux1 + 2816*aux2 + 2815) // 1028522 + aux1 + 1
-		year = 2820*cycle + ycycle + 474
-		if year <= 0 :
+			ycycle = (
+				2134 * aux1
+				+ 2816 * aux2
+				+ 2815
+			) // 1028522 + aux1 + 1
+		year = 2820 * cycle + ycycle + 474
+		if year <= 0:
 			year -= 1
 		yday = jd - to_jd(year, 1, 1) + 1
 		month, day = getMonthDayFromYdays(yday)
-	elif alg==0:## 33-years
+	elif alg == ALG33:
 		jdays = int(jd - GREGORIAN_EPOCH - 584101)
-		## -(1600*365 + 1600//4 - 1600//100 + 1600//400) + 365    -79 +1== -584101
-		#print('jdays =',jdays)
-		j_np = jdays // 12053
-		jdays %= 12053
-		year = 979 + 33*j_np + 4*(jdays//1461)
-		jdays %= 1461
+		# -(1600*365 + 1600//4 - 1600//100 + 1600//400) + 365-79+1 == -584101
+		# log.debug("jdays =", jdays)
+		j_np, jdays = divmod(jdays, 12053)
+
+		yearFact, jdays = divmod(jdays, 1461)
+		year = 979 + 33 * j_np + 4 * yearFact
+
 		if jdays >= 366:
-			year += (jdays-1) // 365
-			jdays = (jdays-1) % 365
-		yday = jdays+1
+			yearPlus, jdays = divmod(jdays - 1, 365)
+			year += yearPlus
+		yday = jdays + 1
 		month, day = getMonthDayFromYdays(yday)
 	else:
-		raise RuntimeError('bad option alg=%s'%alg)
+		raise RuntimeError("bad option alg=%s" % alg)
 	return year, month, day
 
 
-## Normal: esfand = 29 days
-## Leap: esfand = 30 days
+# Normal: esfand = 29 days
+# Leap: esfand = 30 days
 
 def getMonthLen(year, month):
-	if month==12:
+	if month == 12:
 		return 29 + isLeap(year)
 	else:
-		return monthLen[month-1]
-
+		return monthLen[month - 1]
